b6e2df4634af7d8bb687c876b485e3f81d81a57f
AndreyRiazanovv <andreytest112@gmail.com>
2023-11-11 10:34:14 +0200

    second version

--- cypress/e2e/1-getting-started/homework10.cy.js
+++ cypress/e2e/1-getting-started/homework10.cy.js
@@ -1,11 +1,13 @@
     /// <reference types="cypress" />
 
-    import {ApiHelper, apiHelper} from "../../support/apiHelper";
+    import {ApiHelper} from "../../support/apiHelper";
     
     describe("10homeworkLogin" , () => {
     beforeEach(() => {
     cy.loginByAPI();
     });
+
+     // 3 частина 
 
     it("First" , () => {
     cy.contains("Огляд").click();
@@ -14,6 +16,9 @@
     });
      });
      
+     // 4 Додатково вручну створити декілька категорій товарів у меню Асортимент, 
+     // створити функцію, яка буде через API отримувати список цих категорій.
+
     it("Second" , () => {
     cy.contains("Асортимент").click();
     apiHelper.getCategories().then(categories => {
@@ -23,19 +28,16 @@
     
     
     });
-// 5 Перевірка створених категорій
+// 5 Написати тест, який буде використовувати функцію (команду) з п3 для переходу 
+// на сторінку overview, отримувати список створених категорій, використовуючи функцію (команду) 
+// з п4, тобто через API, також переходити через юай у меню Асортимент і валідувати, що кількість 
+// елементів списку на юаї відповідає кількості категорій, отриманих через API запит.
 
 
-describe('CheckCategories', () => {
-    beforeEach(() => {
-    cy.navigateToOverviewPage();
-    });
-    it('Перевірка кількості категорій через API та інтерфейс', () => {
-    cy.getCategories().then((apiResponse) => {
-    const apiCategoryCount = apiResponse.body.length;
-    // Отримати кількість категорій через інтерфейс та валідувати
-    cy.get('.your-category-selector').should('have.length', apiCategoryCount);
-    });
-    });
-    });
+    it('CheckCategories', () => {
+    cy.contains("Асортимент").click();
+    cy.contains("Додади категорія").click();
+    cy.get("input[type='file']").selectFile("cypress/media/photo.jpeg", {forse: true});
+});
+
         
